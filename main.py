c77716dd6e926c7c06ac3cab8d4d431328e66b2d
Arturo Suárez <manuel.suarez@cimat.mx>
2024-07-28 21:58:22 -0600

    Save instead of show because unattended cluster execution

--- main.py
+++ main.py
@@ -145,29 +145,29 @@ plt.plot(history.history["loss"])
 plt.title("Training Loss")
 plt.ylabel("loss")
 plt.xlabel("epoch")
-plt.show()
+plt.savefig('results/training_loss.png')
 
 plt.plot(history.history["accuracy"])
 plt.title("Training Accuracy")
 plt.ylabel("accuracy")
 plt.xlabel("epoch")
-plt.show()
+plt.savefig('results/training_acc.png')
 
 plt.plot(history.history["val_loss"])
 plt.title("Validation Loss")
 plt.ylabel("val_loss")
 plt.xlabel("epoch")
-plt.show()
+plt.savefig('results/validation_loss.png')
 
 plt.plot(history.history["val_accuracy"])
 plt.title("Validation Accuracy")
 plt.ylabel("val_accuracy")
 plt.xlabel("epoch")
-plt.show()
+plt.savefig('results/validation_acc.png')
 
 # Loading the Colormap
 colormap = loadmat(
-    "./instance-level_human_parsing/instance-level_human_parsing/human_colormap.mat"
+    os.path.join(HOME_DIR, "data", "instance-level_human_parsing/instance-level_human_parsing/human_colormap.mat")
 )["colormap"]
 colormap = colormap * 100
 colormap = colormap.astype(np.uint8)
@@ -200,26 +200,26 @@ def get_overlay(image, colored_mask):
     return overlay
 
 
-def plot_samples_matplotlib(display_list, figsize=(5, 3)):
+def plot_samples_matplotlib(display_list, figname, figsize=(5, 3)):
     _, axes = plt.subplots(nrows=1, ncols=len(display_list), figsize=figsize)
     for i in range(len(display_list)):
         if display_list[i].shape[-1] == 3:
             axes[i].imshow(keras.utils.array_to_img(display_list[i]))
         else:
             axes[i].imshow(display_list[i])
-    plt.show()
+    plt.savefig(figname)
 
 
-def plot_predictions(images_list, colormap, model):
-    for image_file in images_list:
+def plot_predictions(images_list, colormap, name, model):
+    for idx, image_file in enumerate(images_list):
         image_tensor = read_image(image_file)
         prediction_mask = infer(image_tensor=image_tensor, model=model)
         prediction_colormap = decode_segmentation_masks(prediction_mask, colormap, 20)
         overlay = get_overlay(image_tensor, prediction_colormap)
         plot_samples_matplotlib(
-            [image_tensor, overlay, prediction_colormap], figsize=(18, 14)
+            [image_tensor, overlay, prediction_colormap], os.path.join("results", f"{name}_{idx}.png"), figsize=(18, 14)
         )
 
-plot_predictions(train_images[:4], colormap, model=model)
-plot_predictions(val_images[:4], colormap, model=model)
+plot_predictions(train_images[:4], colormap, "training", model=model)
+plot_predictions(val_images[:4], colormap, "validation", model=model)
 print("Done!")
